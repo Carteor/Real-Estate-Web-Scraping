0c74dac1e192fd8dd948901b79fe39ae1fa6649f
Carteorcure <carteorcure@gmail.com>
2023-09-24 15:17:55 +0600

    add pagination and save to csv

--- main.py
+++ main.py
@@ -1,16 +1,70 @@
-# This is a sample Python script.
+import requests
+from bs4 import BeautifulSoup
+import csv
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+base_url = "https://krisha.kz/prodazha/kvartiry/almaty/?page="
 
+num_pages = 5
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+listings = []
 
+for page in range(1, num_pages + 1):
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    url = base_url + str(page)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        print(f"Successfully retrieved page {page}.")
+    else:
+        print(f"Failed to retrieve page {page}.")
+        continue
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    listing_elements = soup.find_all('div', attrs={'data-product-id': True})
+
+    print(len(listing_elements))
+
+    for element in listing_elements:
+        #print(element)
+
+        #Square footage
+        title = element.find('a', class_='a-card__title').text.strip()
+        #Listing URL
+        listing_url = 'https://krisha.kz' + element.find('a', class_='a-card__title')['href'].strip()
+        #Property price
+        price = element.find('div', class_='a-card__price').text.strip()
+        #Location(address, city, state)
+        location = element.find('div', class_='card-stats__item').text.strip()
+        address = element.find('div', class_='a-card__subtitle').text.strip()
+        text = element.find('div', class_='a-card__text-preview').text.strip()
+
+        # print(f'Title: {title} \nListing URL: {listing_url}, \nPrice: {price} \nLocation: {location} '
+        #       f'\nAddress: {address} \nText: {text}\n\n')
+
+        listing = {
+            'Title': title,
+            'Listing URL': listing_url,
+            'Price': price,
+            'Location': location,
+            'Address': address,
+        }
+
+        listings.append(listing)
+
+print('Scraping ended')
+
+csv_file_name = 'listings.csv'
+
+csv_header = ['Title', 'Listing URL', 'Price', 'Location', 'Address']
+
+with open(csv_file_name, 'w', newline='', encoding='utf-8') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=csv_header)
+
+    writer.writeheader()
+
+    for listing in listings:
+        writer.writerow(listing)
+
+print(f'Data has been saved to {csv_file_name}.')
